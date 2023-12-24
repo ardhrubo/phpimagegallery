c89afc09d37dbce57c0141baa61460cba4e2b402
ardhrubo <ardhrubo3038@gmail.com>
2023-12-24 17:46:21 +0600

    added date

--- date/date.php
+++ date/date.php
@@ -1,4 +1,13 @@
 <?php
-echo 'hello world';
+
+$date = date("Y");
+
+date_default_timezone_set("Asia/Dhaka");
+
+
+echo "Today is {$date} <br>";
+echo "Today is " . date("h:i:sa"). "<br>";
+echo "hello";
+
 
 ?>
