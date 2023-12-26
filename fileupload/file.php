d477cabad9ac0d06a3ac7b3ab3ad3185f81be7ea
ardhrubo <ardhrubo3038@gmail.com>
2023-12-26 19:42:39 +0600

    file

--- fileupload/file.php
+++ fileupload/file.php
@@ -9,17 +9,32 @@
    <pre>
     <?php 
         print_r($_FILES);
-       move_uploaded_file($_FILES['pdf']['tmp_name'],'./fileupload/uploads/'.$_FILES['pdf']['name']);
+      
 
     
+    if($_FILES['photo']){
+        $allowed = ['image/jpeg','image/png','image/jpg'];
+
+        if(in_array($_FILES['photo']['type'],$allowed)){
+
+            move_uploaded_file($_FILES['photo']['tmp_name'],'./fileupload/uploads/'.$_FILES['photo']['name']);
+            
+        }  else {
+            echo "This is not a right file type, only jpg,jpeg,png allowed";
+        }
+
+    }
+ 
+
     ?>
  
+
 
    </pre>
 
 
     <form action="" method="post" enctype="multipart/form-data">  
-        <input type = "file" name="pdf">
+        <input type = "file" name="photo">
         <input type="submit" name="submit" value="submit">
     </form> 
     
