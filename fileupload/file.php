1d7a1fb6cee6c94f550777c7bdbb210492d642b8
ardhrubo <ardhrubo3038@gmail.com>
2023-12-26 17:22:56 +0600

    file system

---
+++ fileupload/file.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <pre>
+    <?php print_r($_FILES);
+       move_uploaded_file($_FILES['pdf']['tmp_name'],'./fileupload/uploads/abc.pdf')
+    ?>
+ 
+
+   </pre>
+
+
+    <form action="" method="post" enctype="multipart/form-data">  
+        <input type = "file" name="pdf">
+        <input type="submit" name="submit" value="submit">
+    </form> 
+    
+    
+</body>
+</html>
+
+
+
--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-require './date/exper.php';
-include './date/date.php';
-echo readfile("./date/read.txt")
+include('./fileupload/file.php');
 
 
-?>
