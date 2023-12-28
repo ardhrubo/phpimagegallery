8134b5d9df8d1b6178ee8c64bcb90b3ee69f28f5
ardhrubo <ardhrubo3038@gmail.com>
2023-12-28 22:20:44 +0600

    indentation corrected

--- fileupload/file.php
+++ fileupload/file.php
@@ -19,54 +19,44 @@
 <pre>
     <?php 
 
-print_r($_FILES);
+        print_r($_FILES);
 
-// print_r($_POST);
+        // print_r($_POST);
+                
+        // print_r($_SERVER);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if($_FILES['photo']){
         
-// print_r($_SERVER);
+                foreach($_FILES['photo']['tmp_name'] as $key => $tmp_name ){
+                
+                
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if($_FILES['photo']){
- 
-        foreach($_FILES['photo']['tmp_name'] as $key => $tmp_name ){
-          
-        
+                $allowed = ['image/jpeg','image/png','image/jpg'];
 
-        $allowed = ['image/jpeg','image/png','image/jpg'];
+                    if(!in_array($_FILES['photo']['type'][$key],$allowed)){
+                        echo 'File type not supported. Only jpg,jpeg and png are allowed';
+                        exit;
+                    
+                    }
+                    if($_FILES['photo']['size'][$key]> 1020*1024){
+                        echo 'File size should be less than 1 mb';
+                        exit;
+                    }
+            
+                move_uploaded_file($tmp_name,'./fileupload/uploads/'.$_FILES['photo']['name'][$key]);
+            
+                }
 
-        if(!in_array($_FILES['photo']['type'][$key],$allowed)){
-            echo 'File type not supported. Only jpg,jpeg and png are allowed';
+            }   
 
+        } else {
+            echo 'please fill up the form';
             exit;
-          
         }
-        if($_FILES['photo']['size'][$key]> 1020*1024){
-            echo 'File size should be less than 1 mb';
-            exit;
-        }
-    
-
-
-        move_uploaded_file($tmp_name,'./fileupload/uploads/'.$_FILES['photo']['name'][$key]);
-    
-    }
         
-
-    }   
-
-} else {
-    echo 'please fill up the form';
-    exit;
-}
-  
-?>
- 
-
- 
-
-
-
-   </pre>
+    ?>
+</pre>
 
 
 
