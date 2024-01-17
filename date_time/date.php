4c6b9cbd93005c419c4263cda411c4f578902290
ardhrubo <ardhrubo3038@gmail.com>
2024-01-17 21:31:04 +0600

    date

--- date_time/date.php
+++ date_time/date.php
@@ -1,7 +1,18 @@
 <?php
 
-echo date('d-m-y');
-echo time();
+
+// date_default_timezone_set('Asia/Dhaka');
+
+// echo date('d-m-y',mktime(12,0,0,1,16,2021));
+
+$startDate = strtotime('Friday');
+$endDate = strtotime('+1 week');
+
+
+while($startDate< $endDate){
+    echo date("d-m-Y", $startDate). "\n";
+    $startDate = strtotime("+1 day", $startDate);
+}
 
 
 
