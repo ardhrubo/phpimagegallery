5d2158a9cdca30212d7dbf0bf182efcb049ca3ec
ardhrubo <ardhrubo3038@gmail.com>
2024-01-17 21:36:49 +0600

    date time

--- date_time/date.php
+++ date_time/date.php
@@ -5,14 +5,16 @@
 
 // echo date('d-m-y',mktime(12,0,0,1,16,2021));
 
-$startDate = strtotime('Friday');
-$endDate = strtotime('+1 week');
+$startDate = strtotime("2024-01-01");
+$endDate = strtotime("2024-01-20");
 
 
-while($startDate< $endDate){
-    echo date("d-m-Y", $startDate). "\n";
-    $startDate = strtotime("+1 day", $startDate);
-}
+// while($startDate< $endDate){
+//     echo date("d-m-Y", $startDate). "\n";
+//     $startDate = strtotime("+1 day", $startDate);
+// }
 
+$diff = $endDate - $startDate;
 
+echo "Difference between two dates: " . floor(($diff)/(60*60*24)). " Days";
 
