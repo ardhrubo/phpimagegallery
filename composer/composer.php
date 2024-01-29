b46d47fbd7ab5bb29ad6c2473e5b08463d5d3e0e
ardhrubo <ardhrubo3038@gmail.com>
2024-01-29 19:42:24 +0600

    added composer carbon

--- composer/composer.php
+++ composer/composer.php
@@ -1,3 +1,41 @@
 <?php
-echo 'Hello';
+
+require_once 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
+date_default_timezone_set('Asia/Dhaka');
+
+$c = Carbon::now();
+
+
+//echo $c ->timezoneName;
+
+//echo $c -> dayOfWeek;
+//
+//$c->year = 2021;
+//$c->month = 4;
+//echo $c->toDateString();
+
+// eq neq gt lt gte lte
+
+
+$startDate = Carbon::today();
+$endDate = Carbon::now()->addYears(2);
+
+echo 'My age is '.$startDate->diffInMinutes($endDate).' year <br> ';
+
+$Ad = Carbon::parse('-1 year');
+
+echo $Ad;
+
+
+
+
+
+
+
+
+
+
 
